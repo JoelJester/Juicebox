8ad719d40430f1c85c67358745023a88c3d2e58b
Your Name <you@example.com>
2017-04-08 15:13:43 -0400

    some js structure laid out, no real functionality yet.

--- app.js
+++ app.js
@@ -5,15 +5,61 @@
 var Juicebox = {
   songs: [],
   currentSong: null,
-  isPlaying: false,
   dom: {},
+  isPlaying: false,
+  volumeLevel: 100,
 
   // initialization function
   start: function() {
+    this.dom = {
+      play: $(".js-song-controls .play"),
+      stop: $(".js-song-controls .stop"),
+      skip: $(".js-song-controls .skip"),
+      previous: $(".js-song-controls .previous"),
+      mute: $(".js-song-controls .mute"),
+      // this.pause: $(".js-song-controls pause"),
+    };
+    this.listen();
+  },
 
-  }
+  listen: function() {
+    this.dom.play.on("click", this.play);
+    this.dom.stop.on("click", this.stop);
+    this.dom.skip.on("click", this.skip);
+    this.dom.previous.on("click", this.previous);
+    this.dom.mute.on("click", function() {
+      this.setVolume(0);
+    }.bind(this));
+  },
 
-}
+  play: function(song) {
+    console.log("Juicebox is playing");
+    // this.currentSong = song;
+  },
+  pause: function() {
+    console.log("Juicebox is pauseing");
+  },
+  stop: function() {
+    console.log("Juicebox is stoping");
+  },
+  change: function(song) {
+    console.log("Juicebox is changeing");
+  },
+  shuffle: function() {
+    console.log("Juicebox is shuffleing");
+  },
+  skip: function() {
+
+  },
+  setVolume: function(volume) {
+    volumeLevel = volume;
+    console.log("Volume is " + volumeLevel);
+  },
+
+};
+
+
 $(document).ready(function() {
   Juicebox.start();
-})
+  console.log("app linked");
+});
