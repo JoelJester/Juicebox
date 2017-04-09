108f678219b0f854608a41d69861844b49a9e1be
Your Name <you@example.com>
2017-04-09 09:15:34 -0400

    some basic scaffolding and minor styling.

--- app.js
+++ app.js
@@ -15,51 +15,70 @@ var Juicebox = {
       play: $(".js-song-controls .play"),
       stop: $(".js-song-controls .stop"),
       skip: $(".js-song-controls .skip"),
-      previous: $(".js-song-controls .previous"),
+      back: $(".js-song-controls .back"),
       mute: $(".js-song-controls .mute"),
       // this.pause: $(".js-song-controls pause"),
     };
+    this.addSong();
     this.listen();
   },
 
   listen: function() {
-    this.dom.play.on("click", this.play);
-    this.dom.stop.on("click", this.stop);
-    this.dom.skip.on("click", this.skip);
-    this.dom.previous.on("click", this.previous);
+    this.dom.play.on("click", this.play.bind(this));
+    this.dom.stop.on("click", this.stop.bind(this));
+    this.dom.skip.on("click", this.skip.bind(this));
+    // this.dom.back.on("click", this.back.bind(this));
     this.dom.mute.on("click", function() {
       this.setVolume(0);
     }.bind(this));
   },
 
   play: function(song) {
-    console.log("Juicebox is playing");
+    console.log(this);
     // this.currentSong = song;
+    currentSong.play();
   },
   pause: function() {
+    console.log(this);
     console.log("Juicebox is pauseing");
   },
   stop: function() {
-    console.log("Juicebox is stoping");
+    currentSong.stop();
   },
   change: function(song) {
     console.log("Juicebox is changeing");
   },
   shuffle: function() {
+    // currentSong = songs[Math.floor.rand(songs.length);
+    // change(currentSong);
     console.log("Juicebox is shuffleing");
   },
   skip: function() {
-
+    // if (currentSong < songs.length) songs +1;
+    // currentSong = songs[i+1];
+    // change(currentSong);
   },
   setVolume: function(volume) {
     volumeLevel = volume;
     console.log("Volume is " + volumeLevel);
   },
+  addSong: function(path) {
+    this.songs.push(new Song(path));
+  },
 
 };
+
+class Song {
+  constructor(file, title, artist, art, link) {
+
+  }
+
+}
+
 
 
 $(document).ready(function() {
   Juicebox.start();
+  // debugger;
   console.log("app linked");
 });
